e0255f5f991d316d89f5d33d4ceab6c3ec218351
bevinhuynh <bevinh@uci.edu>
2024-10-06 19:57:29 -0700

    Addedheader with logos

--- frontend_functions/getting_backend_info.js
+++ frontend_functions/getting_backend_info.js
@@ -13,7 +13,6 @@ async function handle_authorization() {
             }
         });
 
-        // If the response is a redirect, navigate the browser to the new URL
         if (response.ok) {
             chrome.tabs.create({ url: response.url });
         }
@@ -25,15 +24,11 @@ async function handle_authorization() {
 
 async function handle_playlist_creation() {
 
-    let input_data = document.getElementsByClassName("input-box");
-   
+    let input_data = document.getElementsByClassName("input-box"); 
     let input_value = input_data[0].value;
-    // console.log(input_value)
-    let subtitle = document.getElementsByClassName("subtitle")
     if (input_value.includes("youtube.com/playlist?list=")) {
         data.playlist_url = input_value;
         try {
-            subtitle.textContent = "Please paste in a valid YouTube playlist link."
             const response = await fetch('http://localhost:1410/start-convert', {
                 method: 'POST',
                 headers: {
@@ -41,14 +36,15 @@ async function handle_playlist_creation() {
                 },
                 body: JSON.stringify(data.playlist_url)
             })
+            return "Converted!"
 
         }
         catch {
-            subtitle.textContent = "Error occured with playlist conversion.";
+            return "Error occued with playlist conversion."
         }
     }
     else {
-        subtitle.textContent = "Invalid playlist link.";
+        return "Invalid playlist link."
     }
 }
 
@@ -63,16 +59,3 @@ async function handle_youtube_authorization() {
 }
 
 export default {handle_authorization, handle_playlist_creation, handle_youtube_authorization}
-
-// document.getElementById("youtube-auth").addEventListener("click", function(){
-//     handle_youtube_authorization();
-// })
-
-
-// document.getElementById("convert").addEventListener("click", function() {
-//     handle_playlist_creation();
-// })
-
-// document.getElementById("start").addEventListener("click", function() {
-//     handle_authorization();
-// })
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import click from '../frontend_functions/test';
 import oAuth from '../frontend_functions/getting_backend_info'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [button_name, setName] = useState("Convert Playlist")
+  const button_name = "Convert Playlist";
+  const [subtitle, setSubtitle] = useState("Please paste in a valid YouTube playlist link.")
 
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className='media-logos'>
+        <img src="../src/assets/youtube-name.svg"className="yt_name_logo" alt="Vite logo" /> 
+        <img src="../src/assets/right-arrow.svg" className="right_arrow_logo"/>
+        <img src="../src/assets/spotifyname.svg" className="spotify_name_logo" alt="React logo" />
+          <h1 className='my_name'>By Bevin Huynh</h1>
+        <a href="https://github.com/bevinhuynh/youtube_to_spotify_converter_withReact">
+          <img src = "../src/assets/githublogo.svg" className="githublogo"/>
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
-      <h1>Youtube to Spotify Converter</h1>
+      </div>
 
+      <h1>Youtube to Spotify Converter</h1>
       <button className="auth_spotify" onClick={oAuth.handle_authorization}>Authorize with Spotify</button> <button className="auth_yt" onClick={oAuth.handle_youtube_authorization}>Authorize with YouTube</button>
-      <h2 className="subtitle" id="subtitle">Please paste in a valid YouTube playlist link.</h2>
+      <h2 className="subtitle" id="subtitle">{subtitle}</h2>
       <input className="input-box" placeholder='Paste here'></input>
       <div className="card">
-        <button onClick={oAuth.handle_playlist_creation}>
+        <button className="convert" onClick={async() => {
+        setSubtitle("Converting...");
+        setSubtitle(await oAuth.handle_playlist_creation())
+        }}>
           {button_name}
         </button>
       </div>
+
 
     </>
   )
